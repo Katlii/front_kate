1f4c04d66e8dbbfc08d86386e0b8ba0e3a64df5e
Katsiaryna Zubaryk <zubaryk.katsiaryna@gmail.com>
2023-04-02 21:22:48 +0200

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,60 +1,17 @@
+# frontend code (Streamlit example)
 import streamlit as st
 import requests
-import pandas as pd
 
-# Set up the Streamlit app
-st.set_page_config(page_title='Book Recommender')
+st.header("Books rec")
 
-# Define the API endpoint
-url = 'https://heroku-book-recommender.herokuapp.com/recommendation'
+selected_books = st.text_input('book')
 
-# Define the function to make the API request and get the recommendations
-def get_recommendations(book_title):
-    # Set up the request parameters
-    params = {'title': book_title}
-
-    # Send the GET request to the API endpoint
-    r = requests.get(url, params=params)
-    st.write(r)
-
-    # Parse the JSON response and return the results
-    if r.status_code == 200:
-        results = r.json()
-        return results['top_10'], results['bottom_10']
-    else:
-        st.error('Error getting recommendations from the API.')
-
-# Define the Streamlit app
-def app():
-    # Set up the app sidebar
-    st.sidebar.title('Book Recommender')
-    book_title = st.sidebar.text_input('Enter a book title', value='1984')
-
-    # Get the book recommendations from the backend API
-    top_10, bottom_10 = [], []
-    if st.sidebar.button('Get Recommendations'):
-        top_10, bottom_10 = get_recommendations(book_title)
-    else:
-        top_10, bottom_10 = [], []
-
-    # Display the book recommendations in the app
-    st.title(f'Top 10 Recommendations for {book_title}:')
-    if top_10:
-        df_top_10 = pd.DataFrame(top_10)
-        st.write(df_top_10)
-    else:
-        st.write('No recommendations found.')
-
-    st.title(f'Bottom 10 Recommendations for {book_title}:')
-    if bottom_10:
-        df_bottom_10 = pd.DataFrame(bottom_10)
-        st.write(df_bottom_10)
-    else:
-        st.write('No recommendations found.')
-
-# Run the Streamlit app
-if __name__ == '__main__':
-    app()
-
-
+if st.button('show rec'):
+    # request recommendations from API
+    response = requests.get('http://localhost:5000/recommendations', params={'book': selected_books})
+    data = response.json()
+    
+    # display recommendations
+    recommendations = data['recommendations']
+    st.write(recommendations)
 
