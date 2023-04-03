e84708c33dc31368ac16874988ac16d5f5ca5cc0
Katsiaryna Zubaryk <zubaryk.katsiaryna@gmail.com>
2023-04-04 01:41:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-import requests
 import streamlit as st
+import requests
 import pandas as pd
 
-st.set_page_config(page_title='Book Recommender')
-st.sidebar.title('Book Recommender')
-book_title = st.sidebar.text_input('Enter a book title')
+# Set up the base URL for the Flask API
+BASE_URL = "https://book-recommender-kate.herokuapp.com/"
 
-if st.sidebar.button('Get Recommendations'):
-    response = requests.post('https://book-recommender-kate.herokuapp.com/', json={'book_title': book_title})
-    st.write(response)
-    recommendations = response.json()
-    
-    st.title(f'Top 10 Recommendations for {book_title}:')
-    if not recommendations['top_10']:
-        st.write('No recommendations found.')
+# Define a function to make the API request and get recommendations
+def get_recommendations(book_title):
+    # Define the endpoint and the request body
+    endpoint = f"{BASE_URL}/recommend_books"
+    body = {"book_title": book_title}
+    # Make the POST request to the endpoint
+    response = requests.post(endpoint, json=body)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Get the response data as a dictionary
+        data = response.json()
+        # Convert the dictionary to pandas dataframes
+        top_recs = pd.DataFrame.from_dict(data["top_recommendations"])
+        worst_recs = pd.DataFrame.from_dict(data["worst_recommendations"])
+        # Return the recommendations
+        return top_recs, worst_recs
     else:
-        df_top_10 = pd.DataFrame(recommendations['top_10'])
-        st.write(df_top_10)
+        # If the request failed, show an error message
+        st.error(f"Request failed with status code {response.status_code}")
 
-    st.title(f'Bottom 10 Recommendations for {book_title}:')
-    if not recommendations['bottom_10']:
-        st.write('No recommendations found.')
-    else:
-        df_bottom_10 = pd.DataFrame(recommendations['bottom_10'])
-        st.write(df_bottom_10)
+# Set up the Streamlit app
+st.title("Book Recommendations")
+book_title = st.text_input("Enter a book title")
+if book_title:
+    # Get the recommendations for the book title
+    top_recs, worst_recs = get_recommendations(book_title)
+    # Display the recommendations
+    st.subheader("Top Recommendations")
+    st.table(top_recs)
+    st.subheader("Worst Recommendations")
+    st.table(worst_recs)
+
 
 
