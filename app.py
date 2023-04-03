c804dff87fd90c58f318570ced17b78ade3174cf
Katsiaryna Zubaryk <zubaryk.katsiaryna@gmail.com>
2023-04-04 00:58:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,25 @@ book_title = st.sidebar.text_input('Enter a book title')
 
 if st.sidebar.button('Get Recommendations'):
     response = requests.post('https://book-recommender-kate.herokuapp.com/', json={'book_title': book_title})
-    recommendations = response.json()
+    #recommendations = response.json()
     
     st.title(f'Top 10 Recommendations for {book_title}:')
-    if not recommendations['top_10']:
+    if response.empty:
         st.write('No recommendations found.')
     else:
-        df_top_10 = pd.DataFrame(recommendations['top_10'])
+        df_top_10 = pd.DataFrame(result)
         st.write(df_top_10)
+    #if not recommendations['top_10']:
+        #st.write('No recommendations found.')
+    #else:
+        #df_top_10 = pd.DataFrame(recommendations['top_10'])
+        #st.write(df_top_10)
 
-    st.title(f'Bottom 10 Recommendations for {book_title}:')
-    if not recommendations['bottom_10']:
-        st.write('No recommendations found.')
-    else:
-        df_bottom_10 = pd.DataFrame(recommendations['bottom_10'])
-        st.write(df_bottom_10)
+    #st.title(f'Bottom 10 Recommendations for {book_title}:')
+    #if not recommendations['bottom_10']:
+        #st.write('No recommendations found.')
+    #else:
+        #df_bottom_10 = pd.DataFrame(recommendations['bottom_10'])
+        #st.write(df_bottom_10)
 
 
